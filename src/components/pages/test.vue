00de9669beb710eef745025048b8343b9aef7476
Phoenix-0921 <jyu85921@gmail.com>
2023-07-31 11:44:37 +0800

    copy

--- src/components/pages/test.vue
+++ src/components/pages/test.vue
@@ -1,108 +1,149 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <form class="form-inline" @submit.prevent="connect">
-                    <div class="form-group">
-                        <label for="connect">WebSocket 連接:</label>
-                        <button id="connect" class="btn btn-default" :disabled="connected" type="submit">連接</button>
-                        <button id="disconnect" class="btn btn-default" :disabled="!connected" type="button"
-                            @click="disconnect">關閉連接</button>
-                    </div>
-                </form>
-            </div>
-            <div class="col-md-6">
-                <form class="form-inline" @submit.prevent="sendName">
-                    <div class="form-group">
-                        <label for="name">發送訊息</label>
-                        <input v-model="message" type="text" id="name" class="form-control" placeholder="請輸入訊息">
-                    </div>
-                    <button id="send" class="btn btn-default">發送</button>
-                </form>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <table id="conversation" class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>我的聊天室</th>
-                        </tr>
-                    </thead>
-                    <tbody id="chatRoom">
-                        <tr v-for="(message) in chatMessages" :key="index">
-                            <td>{{ message }}</td>
-                        </tr>
-
-                    </tbody>
-                </table>
-            </div>
-        </div>
+    <div class="menu">
+      <a href="#" class="back"><i class="fa fa-angle-left"></i></a>
+      <div class="name">Random chat</div>
+      <div class="members">You</div>
+  
     </div>
-</template>
-
-<script>
-import { Client } from '@stomp/stompjs';
-export default {
-    name: 'Test',
+  
+    <ol class="chat">
+      <li class="other">
+        <div class="msg">
+          對方傳的內容
+          <time>時間戳記</time>
+        </div>
+      </li>
+      <button @click="connect" v-if="!connected">連接</button>
+      <button @click="disconnect" v-if="connected">斷開連接</button>
+      <li class="self">
+        <div class="msg">
+          <p>我傳送的內容</p>
+          <time>時間戳記</time>
+        </div>
+      </li>
+      <ul>
+        <li v-for="message in messages" :class="message.sender">
+          <div class="msg">
+            <p>{{ message.content }}</p>
+            <time>{{ message.timestamp }}</time>
+          </div>
+        </li>
+      </ul>
+  
+    </ol>
+    <div class="typezone">
+      <div class="typezone">
+        <form>
+          <textarea type="text" placeholder="Say something" v-model="newMessage" @keydown.enter.prevent="sendMessage">
+        </textarea>
+          <input type="submit" class="send" value="" @click="sendMessage($event)" />
+        </form>
+      </div>
+    </div>
+  </template>
+  <script>
+  
+  export default {
+    name: 'test1',
     data() {
-        return {
-            connected: false,
-            
-            message: '',
-            chatMessages: [],
-            heartbeatIntervalId: null, // 心跳間隔的計時器 ID
-        };
+      // 這裡出現的變數才可以在網頁內使用
+      return {
+        showWarning: false, // 開關警告訊息用
+        messages: [], // 存儲聊天消息的數組
+        newMessage: "", // 用於綁定輸入框的值
+        result: "",
+        connected: false,
+        stompClient: null,
+        chatMessages: [],
+        heartbeatIntervalId: null, // 心跳間隔的計時器 ID
+      }
     },
+    //新增watch方法
+    watch: {
+      // 監看
+    },
+  
+    //vue 的方法放這裡
     methods: {
-        connect() {
-            const client = new Client({
-                brokerURL: 'ws://localhost:5173/ws',
-                onConnect: () => {
-                    this.connected = true;
-                    console.log('Connected to WebSocket server');
-
-                    // 訂閱 '/topic/getResponse' 頻道
-                    client.subscribe('/topic/getResponse', message => {
-                        console.log(`Received: ${message.body}`);
-                        const responseMessage = JSON.parse(message.body).responseMessage;
-                        this.showConversation(responseMessage);
-                    });
-
-                    // 發送第一則訊息
-                    client.publish({ destination: '/topic/getResponse', body: 'First Message' });
-                },
-            });
-
-            client.activate(); // 啟動 WebSocket 連接
-        },
-
-        disconnect() {
-            if (this.stompClient !== null) {
-                this.stompClient.disconnect();
-            }
-            this.connected = false;
-            console.log("Disconnected");
-        },
-        // 移除 sendName 方法中的 showConversation 呼叫
-        sendName() {
-            var name = this.message;
-            this.message = '';  // 清空輸入框的內容
-            console.log(name);
-            console.log(this.stompClient);
-            this.stompClient.send("/app/messageControl", {}, JSON.stringify({ 'message': name }));
-        },
-        showConversation(responseMessage) {
-            if (responseMessage && !this.chatMessages.includes(responseMessage)) {
-                this.chatMessages.push(responseMessage);
-            }
+      // matchUsers() {
+      //   this.showWarning = true; // 顯示提示訊息，表示正在等待配對
+      //   // 發送POST請求到後端的隨機配對API
+      //   console.log("我點下了按鈕");
+      //   axios.defaults.baseURL = import.meta.env.VITE_API_HOST;
+      //   axios.post('/chat/match')
+      //     .then(response => {
+      //       // 在前端處理後端回傳的數據
+      //       if (response.data.success) {
+      //         this.result = `用戶 ${response.data.user1Id} 和用戶 ${response.data.user2Id} 成功匹配，可以進行匿名聊天！`;
+      //       } else {
+      //         this.result = '目前找不到足夠的用戶進行配對，請稍後再試。';
+      //       }
+      //     })
+      //     .catch(error => {
+      //       console.error('錯誤:', error);
+      //       this.result = '發生錯誤，請稍後再試。';
+      //     })
+      //     .finally(() => {
+      //       this.showWarning = false; // 配對完成或出現錯誤時，關閉提示訊息
+      //     });
+      // },
+      connect() {
+        var socket = new SockJS('http://localhost:8080/ws');
+        this.stompClient = Stomp.over(socket);
+        var connectHeaders = {
+          'client-id': '10',
+        };
+        this.stompClient.connect(connectHeaders, (frame) => {
+          this.connected = true;
+          console.log(connectHeaders);
+          console.log('Connected: ' + frame);
+          this.stompClient.subscribe('/topic/getResponse', (response) => {
+            console.log(response);
+            const responseMessage = JSON.parse(response.body).responseMessage;
+            this.showConversation(responseMessage);
+          });
+        });
+      },
+  
+      disconnect() {
+        if (this.stompClient !== null) {
+          this.stompClient.disconnect();
         }
-
+        this.connected = false;
+        console.log('Disconnected');
+      },
+  
+  
+      sendName() {
+        var name = this.newMessage;
+        console.log(name);
+        this.newMessage = ''; // 清空輸入框的內容
+        console.log(this.stompClient);
+        this.stompClient.send("/app/messageControl", {}, JSON.stringify({ 'message': name }));
+      },
+      sendMessage(event) {
+        event.preventDefault();
+        if (this.newMessage.trim() !== "") {
+          this.messages.push({
+            sender: "self",
+            content: this.newMessage,
+            timestamp: new Date().toLocaleTimeString()
+          });
+          this.sendName(); // 在這裡調用 `sendName` 方法
+        }
+      },
+      showConversation(responseMessage) {
+        if (responseMessage && !this.chatMessages.includes(responseMessage)) {
+          this.messages.push({
+            sender: 'other', // 假設從後端收到的是對方的訊息，因此設為'other'
+            content: responseMessage,
+            timestamp: new Date().toLocaleTimeString(),
+          });
+        }
+      }
     }
-
-}
-
-
-</script>
-
-<style scoped></style>
+  }
+  
+  </script>
+  <style scoped> @import '/src/css/chatroom.css';
+  </style>
